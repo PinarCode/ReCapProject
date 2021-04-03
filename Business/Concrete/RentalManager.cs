77ec2d3d486261bfd14786a6051e880f7a234551
PINAR KAYA <kayapinar4706@gmail.com>
2021-04-03 18:27:31 +0300

    12. Ders Ödev1

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -27,7 +27,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Rental>> GetAll()
         {
-            if (DateTime.Now.Hour == 17)
+            if (DateTime.Now.Hour == 13)
             {
                 return new ErrorDataResult<List<Rental>>(Messages.MaintenanceTime);
             }
